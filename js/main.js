5924dec19be69ff3d60b6ce99c3e1a4ef781491c
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2019-09-10 16:14:55 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
     $('.box-carousel').slick({
      dots: false,
      arrows: true,
+     infinite: false,
      slidesToShow: 5,
      slidesToScroll: 4,
      prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
