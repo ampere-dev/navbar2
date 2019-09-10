3288048918debfac1945b965dc99ebaa6ff9fe09
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2019-09-10 18:02:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
      infinite: false,
      centerMode: true,
      slidesToShow: 5,
-     slidesToScroll: 4,
+     slidesToScroll: 1,
      prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
      nextArrow: "<button type='button' class='mission-next-arrow'><i class='fa fa-3x fa-chevron-circle-right'></i></button>"
  });
