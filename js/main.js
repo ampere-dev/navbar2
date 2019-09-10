640adb0d2be64d16a7cd9edca99b9ee2ca400bf1
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2019-09-10 08:50:14 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $('.box-carousel').slick({
+     dots: false,
+     arrows: true,
+     slidesToShow: 5,
+     slidesToScroll: 4,
+     prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
+     nextArrow: "<button type='button' class='mission-next-arrow'><i class='fa fa-3x fa-chevron-circle-right'></i></button>"
+ });
+
+});
